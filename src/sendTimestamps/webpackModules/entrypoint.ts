c82bc4552e6855ff8b87a3a295be151509589214
uwx <maxine@cybergal.com>
2025-01-22 19:03:09 -0300

    sendTimestamps 2.0.0: now with non-legacy command

--- src/sendTimestamps/webpackModules/entrypoint.ts
+++ src/sendTimestamps/webpackModules/entrypoint.ts
@@ -1,47 +1,45 @@
 import { Culture, recognizeDateTime } from "@microsoft/recognizers-text-date-time";
+import { CommandType, InputType, OptionType } from "@moonlight-mod/types/coreExtensions/commands";
 import Commands from "@moonlight-mod/wp/commands_commands";
 
 const logger = moonlight.getLogger("sendTimestamps/entrypoint");
 
 logger.info('Hello from sendTimestamps/entrypoint!');
 
-Commands.registerLegacyCommand("uwx-sendTimestamps", {
-  // This can be a more specific regex, but this only tells it to run if it
-  // finds anything that matches this regex within the message.
-  // You will have to do your own extraction and processing if you want to do
-  // something based on a specific string.
-  match: /^\s*\.time(shortdate|longdate|longdateshorttime|full|shorttime|longtime|relative)?\s+(.+)/,
-  action: (content, context) => {
-	const match = content.match(/^\s*\.time(shortdate|longdate|longdateshorttime|full|shorttime|longtime|relative)?\s+(.+)/);
+Commands.registerCommand({
+    id: "time",
+    description: "Sends a date and time in natural language formatted as a timestamp",
+    inputType: InputType.BUILT_IN_TEXT,
+    type: CommandType.CHAT,
+    options: [{
+        name: 'date-and-time',
+        description: 'The date and/or time to send',
+        type: OptionType.STRING,
+    }], // TODO add options for other formats
+    execute: (options) => {
+        const dateAndTime = options[0].value as string;
+        
+        // TODO allow end-user to choose culture
+        const results = recognizeDateTime(dateAndTime, Culture.English);
+        logger.info(results);
+        return {
+            // TODO replace inline in string instead
+            content: results.map(result => formatDate(new Date(result.resolution.values[0].value), 'relative')).join(' '),
+        }
 
-	if (match) {
-		const kind = match[1] || 'relative';
-		const parameters = match[2];
+        function formatDate(date: Date, kind: string) {
+            const unixSeconds = Math.floor(date.getTime() / 1000);
 
-		// TODO allow end-user to choose culture
-		const results = recognizeDateTime(parameters, Culture.English);
-		logger.info(results);
-		return {
-			// TODO replace inline in string instead
-			content: results.map(result => formatDate(new Date(result.resolution.values[0].value), kind)).join(' '),
-		}
-	}
-
-	function formatDate(date: Date, kind: string) {
-		const unixSeconds = Math.floor(date.getTime() / 1000);
-
-		switch (kind) {
-			case 'shortdate': return `<t:${unixSeconds}:d>`;
-			case 'longdate': return `<t:${unixSeconds}:D>`;
-			case 'longdateshorttime': return `<t:${unixSeconds}:f>`;
-			case 'full': return `<t:${unixSeconds}:F>`;
-			case 'shorttime': return `<t:${unixSeconds}:t>`;
-			case 'longtime': return `<t:${unixSeconds}:T>`;
-			case 'relative': return `<t:${unixSeconds}:R>`;
-			default: return `<t:${unixSeconds}:R>`;
-		}
-	}
-
-    return {content};
-  }
+            switch (kind) {
+                case 'shortdate': return `<t:${unixSeconds}:d>`;
+                case 'longdate': return `<t:${unixSeconds}:D>`;
+                case 'longdateshorttime': return `<t:${unixSeconds}:f>`;
+                case 'full': return `<t:${unixSeconds}:F>`;
+                case 'shorttime': return `<t:${unixSeconds}:t>`;
+                case 'longtime': return `<t:${unixSeconds}:T>`;
+                case 'relative': return `<t:${unixSeconds}:R>`;
+                default: return `<t:${unixSeconds}:R>`;
+            }
+        }
+    }
 })
