bc0f5ab9596b976257c7237648e788c49baa1b82
uwx <maxine@cybergal.com>
2025-01-26 23:09:35 -0300

    Include file name and file size in Ruffle preloader

--- src/ruffle/webpackModules/entrypoint/index.tsx
+++ src/ruffle/webpackModules/entrypoint/index.tsx
@@ -6,6 +6,38 @@ const logger = moonlight.getLogger('ruffle/entrypoint');
 
 logger.info('Hello from ruffle/entrypoint!');
 
+// https://stackoverflow.com/a/14919494
+/**
+ * Format bytes as human-readable text.
+ * 
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use 
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ * 
+ * @return Formatted string.
+ */
+function humanFileSize(bytes: number, si = false, dp = 1) {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+        return `${bytes} bytes`;
+    }
+
+    const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10 ** dp;
+
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+    return `${bytes.toFixed(dp)} ${units[u]}`;
+}
+
 export function handleFileEmbed(props: Props) {
     if (props.item.contentType !== 'application/vnd.adobe.flash.movie') {
         return undefined;
@@ -37,6 +69,9 @@ export function handleFileEmbed(props: Props) {
         />
     ) : (
         <button style={{ width: '550px', height: '400px', display: 'block', cursor: 'pointer' }} type="button" tabIndex={0} onClick={() => setShowing(true)}>
+            <span style={{ position: 'absolute', left: '1rem', top: '1rem' }}> 
+                Click to play {props.fileName} ({humanFileSize(props.fileSize)})
+            </span>
             <div style={{
                 height: '50%',
                 left: '50%',
